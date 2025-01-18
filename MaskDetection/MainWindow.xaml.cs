90a5bbc9112282cbd6c1d9128ddd76455961bd67
JUNE-PC\21jun <kjunekjune@gmail.com>
2025-01-18 19:31:07 +0900

    Code source cleanup;

--- MaskDetection/MainWindow.xaml.cs
+++ MaskDetection/MainWindow.xaml.cs
@@ -32,54 +32,64 @@ using System.Runtime.InteropServices;
 
 namespace MaskDetection
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		VideoCapture m_capture;
 		Thread t_cap;
 		bool m_isRunning = false;
-		bool b_facecog = true;
+		bool b_facedet = true;
 
 		OpenCvSharp.Dnn.Net net;
 		OpenCvSharp.Size resz = new OpenCvSharp.Size(224, 224);
 
-		// Mask_12K First
-		bool meanstd = false;
+		// Mean & Standard Deviation
+		// mean, std 구해놨지만 안쓰는게 더 잘 나옴
+		bool b_meanstd = false;
 		static float[] mean = new float[3] { 0.5703f, 0.4665f, 0.4177f };
 		static float[] std = new float[3] { 0.2429f, 0.2231f, 0.2191f };
 
-		float face_confidence = 0.5f;
+		float face_confidence = 0.3f;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 			m_capture = new VideoCapture();
+
+			// Download: https://github.com/21june/PyroNote/blob/main/MaskDetection/resnet18_Mask_12K_None_EPOCH200_LR0.0001.onnx
 			string mask_model = "resnet18_Mask_12K_None_EPOCH200_LR0.0001.onnx";
 			net = OpenCvSharp.Dnn.CvDnn.ReadNetFromOnnx(mask_model);
+
+			// UI 처리
 			if (!net.Empty())	text_model.Text = "Model: " + mask_model;
 			else				text_model.Text = "Model: " + "No Model";
 
 			slider_face_conf.Value = (int)(face_confidence * 100);
-			check_facedet.IsChecked = b_facecog = true;
+			check_facedet.IsChecked = b_facedet = true;
 		}
 
-
-		private void ImageROI(Mat image,  out List<OpenCvSharp.Rect> faces)
+		private void ThreadFunc() // 카메라를 연결하고 프레임을 읽고 추론까지 진행함
 		{
-			faces = new List<OpenCvSharp.Rect>();
+			m_capture.Open(0, VideoCaptureAPIs.DSHOW);
 
-			if (b_facecog == true)
+			Mat frame = new Mat();
+			while (m_isRunning)
 			{
-				FaceCrop(image, out faces);
+				if (m_capture.IsOpened() == true)
+				{
+					m_capture.Read(frame);
+					if (!frame.Empty())
+						Run(frame);
+					Thread.Sleep(10); // prevent for lag
+				}
+				else
+				{
+					m_isRunning = false;
+					image_cam.Dispatcher.Invoke(() => { image_cam.Source = null; });
+					button_cam.Dispatcher.Invoke(() => { button_cam.Content = "Open"; });
+				}
 			}
-			else
-			{
-				OpenCvSharp.Rect rt = new OpenCvSharp.Rect(0, 0, image.Width, image.Height);
-				faces.Add(rt);
-			}
-
+			if (m_capture.IsOpened())
+				m_capture.Release();
 		}
 
 		private void Run(Mat image)
@@ -106,162 +116,32 @@ namespace MaskDetection
 					Cv2.Rectangle(image, rt, Scalar.Red, 3);
 					Cv2.PutText(image, prob.ToString("0.00"), new OpenCvSharp.Point(rt.X, rt.Y), HersheyFonts.HersheyDuplex, 1.0, Scalar.Black);
 				}
+				else
+				{
+					Cv2.Rectangle(image, rt, Scalar.Gray, 3);
+					Cv2.PutText(image, "N/A", new OpenCvSharp.Point(rt.X, rt.Y), HersheyFonts.HersheyDuplex, 1.0, Scalar.Black);
+				}
 			}
 
-			image_cam.Dispatcher.Invoke(() =>
-			{
-				image_cam.Source = OpenCvSharp.WpfExtensions.BitmapSourceConverter.ToBitmapSource(image);
-			});
+			image_cam.Dispatcher.Invoke(() => { image_cam.Source = OpenCvSharp.WpfExtensions.BitmapSourceConverter.ToBitmapSource(image); });
 		}
-		private void Inference(Mat image, out int label, out double prob)
+
+		private void ImageROI(Mat image,  out List<OpenCvSharp.Rect> faces)
 		{
-			if (net.Empty())
-			{
-				MessageBox.Show("No Found Model!");
-				label = -1; prob = 0;
-				return;
-			}
-			if (image.Empty()) {
-				label = -1; prob = 0; return;
-			}
-			Mat resizedImage = image.Clone();
-			Mat blob = new Mat();
-			NormalizeImage(ref resizedImage);
-			blob = CvDnn.BlobFromImage(resizedImage, 1.0f,
-				new OpenCvSharp.Size(224, 224), swapRB:true, crop:false);
+			faces = new List<OpenCvSharp.Rect>();
 
-			net.SetInput(blob);
-			string[] outBlobNames = net.GetUnconnectedOutLayersNames();
-			Mat[] outputBlobs = outBlobNames.Select(toMat => new Mat()).ToArray();
-			Mat matprob = net.Forward("output");
-
-			// Test-Try
-			if (true)
+			if (b_facedet == true)
 			{
-				double maxVal, minVal;
-				OpenCvSharp.Point minLoc, maxLoc;
-				Cv2.MinMaxLoc(matprob, out minVal, out maxVal, out minLoc, out maxLoc);
-				label = maxLoc.X;
-				prob = maxVal * 100;
+				FaceCrop(image, out faces);
 			}
 			else
 			{
-				float[] data = new float[matprob.Total() * matprob.Channels()];
-				matprob.GetArray(out data);
-				float[] result = Softmax(data);
-				Console.WriteLine(string.Join(", ", result));
-				if (result.Length >= 2)
-				{
-					if (result[0] > result[1])
-					{
-						label = 0; prob = result[0] * 100;
-					}
-					else
-					{
-						label = 0; prob = result[1] * 100;
-					}
-				}
-				else
-				{
-					label = 0; prob = 0;
-				}
+				OpenCvSharp.Rect rt = new OpenCvSharp.Rect(0, 0, image.Width, image.Height);
+				faces.Add(rt);
 			}
+
 		}
 
-		private void NormalizeImage(ref Mat img)
-		{
-			img.ConvertTo(img, MatType.CV_32FC3);
-			Mat[] rgb = img.Split();
-			rgb[0] = rgb[0].Divide(255.0f); // B
-			rgb[1] = rgb[1].Divide(255.0f); // G
-			rgb[2] = rgb[2].Divide(255.0f); // R
-			if (meanstd)
-			{
-				rgb[2] = rgb[2].Subtract(new Scalar(mean[0])); // B
-				rgb[1] = rgb[1].Subtract(new Scalar(mean[1])); // G
-				rgb[0] = rgb[0].Subtract(new Scalar(mean[2])); // R
-
-				rgb[2] = rgb[2].Divide(std[0]); // B
-				rgb[1] = rgb[1].Divide(std[1]); // G
-				rgb[0] = rgb[0].Divide(std[2]); // R
-			}
-			Cv2.Merge(rgb, img);
-			Cv2.Resize(img, img, resz);
-		}
-
-		private float[] Softmax(float[] logits)
-		{
-			// Find max logit for numerical stability
-			float maxLogit = logits.Max();
-
-			// Compute exp(logit - maxLogit)
-			float[] exps = logits.Select(l => (float)Math.Exp(l - maxLogit)).ToArray();
-
-			// Sum of exp
-			float sumExp = exps.Sum();
-
-			// Normalize
-			float[] probabilities = exps.Select(e => e / sumExp).ToArray();
-			return probabilities;
-		}
-
-		private void Btn_Click(object sender, RoutedEventArgs e)
-		{
-			if (sender.Equals(button_cam)) // 카메라 연결/해제 과정
-			{
-				if (!m_isRunning)
-				{
-					if (t_cap != null && t_cap.IsAlive)
-					{
-						MessageBox.Show("Camera is closing... Wait..", "Error");
-						return;
-					}
-					t_cap = new Thread(new ThreadStart(Grab));
-					t_cap.IsBackground = true; // 프로그램 꺼질 때 쓰레드도 같이 꺼짐
-					m_isRunning = true;
-					t_cap.Start();
-					button_cam.Content = "Close";
-				}
-				else
-				{
-					m_isRunning = false;
-					image_cam.Source = null;
-					button_cam.Content = "Open";
-				}
-			}
-
-			else if (sender.Equals(button_image)) // 이미지 로드 과정
-			{
-
-				OpenFileDialog openFileDialog = new OpenFileDialog();
-				openFileDialog.Filter = "PNG files (*.png)|*.png|BMP files (*.bmp)|*.bmp|JPG files (*.jpg)|*.jpg|JPEG files (*.jpeg)|*.jpeg|All files (*.*)|*.*";
-				if (openFileDialog.ShowDialog() == true)
-				{
-					Mat image = Cv2.ImRead(openFileDialog.FileName);
-					if (!image.Empty()) text_image.Text = "Image: " + openFileDialog.SafeFileName;
-					else text_image.Text = "Image: " + "No Model";
-					Run(image);
-				}
-			}
-			else if (sender.Equals(button_model))
-			{
-
-				OpenFileDialog openFileDialog = new OpenFileDialog();
-				openFileDialog.Filter = "ONNX Weight files (*.onnx)|*.onnx|All files (*.*)|*.*";
-				if (openFileDialog.ShowDialog() == true)
-				{
-					net = OpenCvSharp.Dnn.CvDnn.ReadNetFromOnnx(openFileDialog.FileName);
-					if (!net.Empty())	text_model.Text = "Model: " + openFileDialog.SafeFileName;
-					else				text_model.Text = "Model: " + "No Model";
-				}
-			}
-
-			else if (sender.Equals(check_facedet))
-			{
-				if (check_facedet.IsChecked == true)	b_facecog = true;
-				else									b_facecog = false;
-			}
-		}
 
 		// face detection for using ssd (or haarcascade)
 		private void FaceCrop(Mat image, out List<OpenCvSharp.Rect> list)
@@ -271,7 +151,9 @@ namespace MaskDetection
 			if (true) // SSD Model : Useful
 			{
 				OpenCvSharp.Dnn.Net facenet;
+				// Download: https://github.com/spmallick/learnopencv/blob/master/FaceDetectionComparison/models/deploy.prototxt
 				var prototext = "deploy.prototxt";
+				// Download: https://github.com/spmallick/learnopencv/blob/master/FaceDetectionComparison/models/res10_300x300_ssd_iter_140000_fp16.caffemodel
 				var modelPath = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
 				facenet = Net.ReadNetFromCaffe(prototext, modelPath);
 				Mat inputBlob = CvDnn.BlobFromImage(
@@ -287,12 +169,13 @@ namespace MaskDetection
 				int cols = outputBlobs.Size(3);
 				long total = outputBlobs.Total();
 				ch1Blobs.GetArray(out float[] data);
-				if (data.Length == 1)
-					return;
+				if (data.Length == 1) return;
+
 				for (int i = 0; i < rows; i++)
 				{
 					float confidence = data[i * cols + 2]; // Access confidence score
 
+					// 설정된 confidence 값보다 클 경우만
 					if (confidence > face_confidence)
 					{
 						int x1 = (int)(data[i * cols + 3] * image.Width);
@@ -311,24 +194,19 @@ namespace MaskDetection
 						// 그냥 face recognition 하면, 얼굴이 너무 빡세게 잡혀서.. 가로세로 10% 정도씩 늘려줌.
 						float face_scale_X = 0.1f;
 						float face_scale_Y = 0.1f;
-						if (x1 - (width * face_scale_X) < 0)				x1 = 0;
-						else												x1 = x1 - (int)(width * face_scale_X);
+						if (x1 - (width * face_scale_X) < 0) x1 = 0;
+						else x1 = x1 - (int)(width * face_scale_X);
 
-						if (x2 + (width * face_scale_X) > image.Width)		x2 = image.Width;
-						else												x2 = x2 + (int)(width * face_scale_X);
+						if (x2 + (width * face_scale_X) > image.Width) x2 = image.Width;
+						else x2 = x2 + (int)(width * face_scale_X);
 
-						if (y1 - (height * face_scale_Y) < 0)				y1 = 0;
-						else												y1 = y1 - (int)(height * face_scale_Y);
+						if (y1 - (height * face_scale_Y) < 0) y1 = 0;
+						else y1 = y1 - (int)(height * face_scale_Y);
 
-						if (y2 + (height * face_scale_Y) > image.Height)	y2 = image.Height;
-						else												y2 = y2 + (int)(height * face_scale_Y);
+						if (y2 + (height * face_scale_Y) > image.Height) y2 = image.Height;
+						else y2 = y2 + (int)(height * face_scale_Y);
 
 						OpenCvSharp.Rect item = new OpenCvSharp.Rect(x1, y1, x2 - x1, y2 - y1);
-						Cv2.Rectangle(image,
-							new OpenCvSharp.Point(item.Left, item.Top),
-							new OpenCvSharp.Point(item.Right, item.Bottom),
-							Scalar.Green, 2, LineTypes.Link8);
-
 						list.Add(item);
 					}
 				}
@@ -337,7 +215,7 @@ namespace MaskDetection
 			if (false) // Cascade Classifier : Useless
 			{
 				// Download: https://github.com/mitre/biqt-face/tree/master/config/haarcascades
-				string filenameFaceCascade = "haarcascade_frontalface_alt2.xml"; 
+				string filenameFaceCascade = "haarcascade_frontalface_alt2.xml";
 				CascadeClassifier faceCascade = new CascadeClassifier();
 				if (!faceCascade.Load(filenameFaceCascade))
 				{
@@ -356,26 +234,122 @@ namespace MaskDetection
 			}
 		}
 
-		private void Grab() // 카메라를 연결하고 프레임을 읽고 추론까지 진행함
+		// 정규화 과정
+		private void NormalizeImage(ref Mat img)
 		{
-			m_capture.Open(0, VideoCaptureAPIs.DSHOW);
+			img.ConvertTo(img, MatType.CV_32FC3);
+			Mat[] rgb = img.Split();
 
-			Mat frame = new Mat();
-			while (m_isRunning)
+			// 0.0f~1.0f
+			rgb[0] = rgb[0].Divide(255.0f); // B
+			rgb[1] = rgb[1].Divide(255.0f); // G
+			rgb[2] = rgb[2].Divide(255.0f); // R
+			if (b_meanstd)
 			{
-				if (m_capture.IsOpened() == true)
+				// mean
+				rgb[2] = rgb[2].Subtract(new Scalar(mean[0])); // B
+				rgb[1] = rgb[1].Subtract(new Scalar(mean[1])); // G
+				rgb[0] = rgb[0].Subtract(new Scalar(mean[2])); // R
+
+				// std
+				rgb[2] = rgb[2].Divide(std[0]); // B
+				rgb[1] = rgb[1].Divide(std[1]); // G
+				rgb[0] = rgb[0].Divide(std[2]); // R
+			}
+			Cv2.Merge(rgb, img);
+			Cv2.Resize(img, img, resz);
+		}
+
+		// 추론 과정
+		private void Inference(Mat image, out int label, out double prob)
+		{
+			if (net.Empty())
+			{
+				MessageBox.Show("No Found Model!");
+				label = -1; prob = 0;
+				return;
+			}
+			if (image.Empty()) {
+				label = -1; prob = 0; return;
+			}
+			Mat resizedImage = image.Clone();
+			Mat blob = new Mat();
+			NormalizeImage(ref resizedImage); 
+			blob = CvDnn.BlobFromImage(resizedImage, 1.0f,
+				new OpenCvSharp.Size(224, 224), swapRB:true, crop:false);
+
+			net.SetInput(blob);
+			string[] outBlobNames = net.GetUnconnectedOutLayersNames();
+			Mat[] outputBlobs = outBlobNames.Select(toMat => new Mat()).ToArray();
+			Mat matprob = net.Forward("output");
+
+			// 최대 값의 구하기
+			double maxVal, minVal;
+			OpenCvSharp.Point minLoc, maxLoc;
+			Cv2.MinMaxLoc(matprob, out minVal, out maxVal, out minLoc, out maxLoc);
+			label = maxLoc.X;
+			prob = maxVal * 100;
+		}
+
+		// 버튼 클릭 이벤트
+		private void Btn_Click(object sender, RoutedEventArgs e)
+		{
+			if (sender.Equals(button_cam)) // 카메라 연결/해제 버튼
+			{
+				if (!m_isRunning)
 				{
-					m_capture.Read(frame);
-					if (!frame.Empty())
-						Run(frame);
-					Thread.Sleep(10); // prevent for lag
+					if (t_cap != null && t_cap.IsAlive)
+					{
+						MessageBox.Show("Camera is closing... Wait..", "Error");
+						return;
+					}
+					t_cap = new Thread(new ThreadStart(ThreadFunc));
+					t_cap.IsBackground = true; // 프로그램 꺼질 때 쓰레드도 같이 꺼짐
+					m_isRunning = true;
+					t_cap.Start();
+					button_cam.Content = "Close";
 				}
 				else
+				{
 					m_isRunning = false;
+					image_cam.Source = null;
+					button_cam.Content = "Open";
+				}
 			}
-			if (m_capture.IsOpened())
-				m_capture.Release();
+
+			else if (sender.Equals(button_image)) // 이미지 로드 버튼
+			{
+
+				OpenFileDialog openFileDialog = new OpenFileDialog();
+				openFileDialog.Filter = "PNG files (*.png)|*.png|BMP files (*.bmp)|*.bmp|JPG files (*.jpg)|*.jpg|JPEG files (*.jpeg)|*.jpeg|All files (*.*)|*.*";
+				if (openFileDialog.ShowDialog() == true)
+				{
+					Mat image = Cv2.ImRead(openFileDialog.FileName);
+					if (!image.Empty()) text_image.Text = "Image: " + openFileDialog.SafeFileName;
+					else text_image.Text = "Image: " + "No Model";
+					Run(image);
+				}
+			}
+			else if (sender.Equals(button_model)) // 모델 로드 버튼
+			{
+
+				OpenFileDialog openFileDialog = new OpenFileDialog();
+				openFileDialog.Filter = "ONNX Weight files (*.onnx)|*.onnx|All files (*.*)|*.*"; // ONNX 파일만
+				if (openFileDialog.ShowDialog() == true)
+				{
+					net = OpenCvSharp.Dnn.CvDnn.ReadNetFromOnnx(openFileDialog.FileName);
+					if (!net.Empty())	text_model.Text = "Model: " + openFileDialog.SafeFileName;
+					else				text_model.Text = "Model: " + "No Model";
+				}
+			}
+			else if (sender.Equals(check_facedet)) // face detection 체크 박스
+			{
+				if (check_facedet.IsChecked == true)	b_facedet = true;
+				else									b_facedet = false;
+			}
 		}
+
+		// Slider 값 변경 이벤트
 		private void ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
 			if (sender.Equals(slider_face_conf))
